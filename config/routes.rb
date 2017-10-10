d1af1545c814cee6ed77512ebc5403afd4f6456e
christopherwperry <christopherwperry@gmail.com>
2017-10-10 11:49:38 -0400

    Check relationship of stop and tour in stop migration table.

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :organizations do
     resources :admin
   end
+  
   resources :tour do
     resources :stop do
       collection do
--- db/migrate/20171009211011_create_stops.rb
+++ db/migrate/20171009211011_create_stops.rb
@@ -1,17 +1,17 @@
 class CreateStops < ActiveRecord::Migration[5.1]
   def change
     create_table :stops do |t|
-      t.integer :stop_num
-      t.string :name
+      t.integer :stop_num, null: false
+      t.string :name, null: false
       t.string :directions_to_next_stop
       t.string :learn_more_URL
       t.text :travel_tip
       t.text :description
-      t.string :location
+      t.string :location, null: false
       t.string :image_current
       t.string :image_historic
-      t.integer :gps_long
-      t.integer :gps_lat
+      t.integer :gps_long, default: 0
+      t.integer :gps_lat, default: 0
       t.string :badge
       t.belongs_to :tour, foreign_key: true
       t.belongs_to :admin, foreign_key: true
