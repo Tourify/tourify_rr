4a2092fc4bc0c326a8fdaee9e14f72426228016e
gingin77 <ginniehench@gmail.com>
2017-10-10 14:06:27 -0400

    Set up the seed data file to create Objects for the Organization, Admin, and Tour classes. The tour#show jbuilder view is done. Next, import the stops

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -1,11 +1,21 @@
 class TourController < ApplicationController
+  # before_action :set_tour
+
   def index
   end
 
   def show
+    @tour = Tour.find(params[:id])
+    render 'show.json'
   end
 
   def create
+    # @tour = Tour.new(tour_params)
+    #
+    # if @tour.save
+    #   redirect_to tour_stop_index_path
+    # else
+    #   redirect_to root
   end
 
   def update
@@ -13,4 +23,13 @@ class TourController < ApplicationController
 
   def destroy
   end
+
+  private
+    def tour_params
+      params.require(:tour).permit(:name, :description, :distance, :time_in_mins)
+    end
+
+  # def set_tour
+  #   @tour =
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+  # Character.create(name: 'Luke', movie: movies.first)
+#
+dvb = Organization.create!(name: "DVB", display_name: "Visitor's Burea")
+
+admin1 = Admin.create!(username: "Ginnie", password: "ducks", organization: dvb)
+
+main_tour = Tour.create!(name: "Main route", description: "This is the main route as defined by the DVB", distance: 1.2, time_in_mins: 60, organization: dvb, admin: admin1)
