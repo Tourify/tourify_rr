175a64b4c671214625507edac05961551084ea56
gingin77 <ginniehench@gmail.com>
2017-10-09 20:53:15 -0400

    I added yarn and webpack files in order to get the server to run. The form now functions to import a csv data file

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,7 +1,10 @@
 class StopsController < ApplicationController
   def index
+    @stops = Stop.all
   end
 
   def import
+    Stop.import(params[:file])
+    redirect_to stops_path, notice: "Data imported"
   end
 end
