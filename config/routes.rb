5b135ff0b54adcf858e491e0cb6e36e8cf68b113
christopherwperry <christopherwperry@gmail.com>
2017-10-09 21:47:39 -0400

    Generate controllers for Admin and Tour. Nest resources in routes.rb to establish desired URL structure for model dependencies.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,34 @@
 Rails.application.routes.draw do
-  get 'stops/index'
 
-  get 'stops/import'
+  resources :admin do
+    resources :tour do
+      resources :stop
+        post :import
+    end
+  end
+  # get 'admin/index'
+  #
+  # get 'admin/show'
+  #
+  # get 'admin/create'
+  #
+  # get 'admin/update'
+  #
+  # get 'admin/destroy'
+  #
+  # get 'tour/index'
+  #
+  # get 'tour/show'
+  #
+  # get 'tour/create'
+  #
+  # get 'tour/update'
+  #
+  # get 'tour/destroy'
+  #
+  # get 'stops/index'
+  #
+  # get 'stops/import'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
