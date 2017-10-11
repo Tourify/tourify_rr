64bbbb3cc921bc65b881f3e0406234a726b6ebcd
christopherwperry <christopherwperry@gmail.com>
2017-10-11 13:21:51 -0400

    Add create and destroy methods to sessions controller. Add routes for register, login, logout for sessions actions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/destroy'
+  get 'signup', to: 'admins#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   get '/', to: 'index#index'
 
