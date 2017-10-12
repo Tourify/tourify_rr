fee15424643947a0dc51169e865759a7e171a474
christopherwperry <christopherwperry@gmail.com>
2017-10-12 10:49:31 -0400

    Add presence validators to Admin, Org, Stop, and Tour models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    include SessionsHelper
+  protect_from_forgery with: :exception
+  include SessionsHelper
 
   # private
   #
