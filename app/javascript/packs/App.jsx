244870278ec200e2103a431f2665ad1ed1f7cfc6
Katy Dillard <durhamdill@gmail.com>
2017-10-12 15:56:22 -0400

    builds Tour Home page

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Baselayout from '../components/Baselayout';
 import TourStop from '../components/TourStop';
+import TourHome from '../components/TourHome';
 import Home from '../components/Home';
 
 
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <BrowserRouter>
       <Baselayout>
       <Switch>
-        <Route exact path = "/" component={TourStop}/>
+        <Route exact path = "/" component={TourHome}/>
       </Switch>
     </Baselayout>
     </BrowserRouter>
