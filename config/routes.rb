79eaf4e0428277ff4680674c8c98d30fbb327527
gingin77 <ginniehench@gmail.com>
2017-10-13 00:19:36 -0400

    Adding functionality buttons/links to the Stops#index page
    To add a downloadable template, I made modifications to the routes file, the stops controller and the stops.index view.
    
    The tours#show page now includes a link to the stops#index page that says 'Add/edit stops in bulk'.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get '/', to: 'index#index'
+  get '/stops/download_template'
 
   resources :sessions
   resources :organizations do
