0b45879401fedef485d82100edd5dcde75561fd0
gingin77 <ginniehench@gmail.com>
2017-10-21 21:49:09 -0400

    Make syntax changes in initializer

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,25 +1,24 @@
 CarrierWave.configure do |config|
 
+  # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
       config.storage = :file
     end
   end
 
+  # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'
       config.storage = :fog
     end
   end
 
   config.fog_credentials = {
-    :provider               'AWS',
-    :aws_access_key_id      'AKIAI7XJRYPQXNL2OVNA',
-    :aws_secret_access_key '4U/DYjj9ljUK9rmnbS8rwmgfUPFhM8tLF9fEbyMQ',
-    :region                'us-east-1',
-    host:                  's3.example.com',
-    endpoint:              'https://s3.example.com:8080'
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAI7XJRYPQXNL2OVNA',
+    aws_secret_access_key: '4U/DYjj9ljUK9rmnbS8rwmgfUPFhM8tLF9fEbyMQ',
+    region: 'us-east-1 '
   }
   config.fog_directory  = 'tourifync'
   config.fog_public     = false
