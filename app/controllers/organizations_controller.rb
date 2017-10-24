20f9dcf7d9d42d42310117e003dd4e5b8303eb16
christopherwperry <christopherwperry@gmail.com>
2017-10-24 10:35:31 -0400

    Add admin validation to stop/organizations actions. Correct organization routing issue for updating tour information.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -38,6 +38,7 @@ class OrganizationsController < ApplicationController
       redirect_to new_session_path
     end
   end
+  
   # def create
   #   @organization = Organization.create(organization_params)
   #   render :show, status: :created
